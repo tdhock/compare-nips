a9821c4457b94184550e6a686f573a732747879e
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-10-04 15:44:22 +0900

    stratify by yi for train/test split

--- figure-simulation.R
+++ figure-simulation.R
@@ -16,12 +16,12 @@ for(norm in names(funs)){
   Xi <- c()
   Xip <- c()
   yi <- c()
-  for(i in 1:500){
+  for(i in 1:1000){
     x <- runif(2,-2,2)
     delta <- delta.fun()
     xp <- x+delta
     noise <- 0
-    noise <- rnorm(1,sd=1/2)#comment for noiseless simulation.
+    noise <- rnorm(1,sd=1)#comment for noiseless simulation.
     fxdiff <- f(xp)-f(x)+noise
     y <- ifelse(fxdiff < -1, -1L,
                 ifelse(fxdiff > 1, 1L, 0L))
@@ -31,34 +31,69 @@ for(norm in names(funs)){
   }
   pair.sets[[norm]] <- list(Xi=Xi, Xip=Xip, yi=yi)
 }
+N <- 200
 lapply(pair.sets, with, table(yi))
-
+## Pick exactly N equality and inequality pairs.
+pairs.picked <- list()
+for(norm in names(pair.sets)){
+  Pairs <- pair.sets[[norm]]
+  is.zero <- Pairs$yi == 0
+  equal <- which(is.zero)
+  not.equal <- which(!is.zero)
+  i <- c(sample(equal, N), sample(not.equal, N))
+  pairs.picked[[norm]] <-
+    list(Xi=Pairs$Xi[i,],
+         Xip=Pairs$Xip[i,],
+         yi=Pairs$yi[i])
+}
+lapply(pairs.picked, with, table(yi))
+## Plot the points.
+point.df <- data.frame()
+for(norm in names(pairs.picked)){
+  m <- with(pairs.picked[[norm]], rbind(Xi, Xip))
+  point.df <- rbind(point.df, data.frame(m, norm, row.names=NULL))
+}
+library(grid)
+ggplot()+
+  geom_point(aes(X1, X2), data=point.df)+
+  facet_grid(.~norm)+
+  theme_bw()+
+  theme(panel.margin=unit(0,"cm"))+
+  coord_equal()
+## Looks fine.
 levs <- seq(-2,2,l=41)
 X.grid <- as.matrix(expand.grid(x1=levs,x2=levs))
 all.ranks <- data.frame()
 ## fit SVM.
-for(norm in names(pair.sets)){
-  Pairs <- pair.sets[[norm]]
-  fold <- sample(rep(1:2, l=length(Pairs$yi)))
-  is.train <- fold==1
+for(norm in names(pairs.picked)){
+  Pairs <- pairs.picked[[norm]]
+  is.zero <- Pairs$yi == 0
+  equal <- which(is.zero)
+  not.equal <- which(!is.zero)
+  i <- c(sample(equal, N/2), sample(not.equal, N/2))
+  is.train <- seq_along(Pairs$yi) %in% i
+  Pairs.train <- list(Xi=Pairs$Xi[is.train,],
+                      Xip=Pairs$Xip[is.train,],
+                      yi=Pairs$yi[is.train])
   err.df <- data.frame()
-  Cvals <- 2^seq(-4,4,by=1)
+  Cvals <- 3^seq(-2,6,by=1)
   models <- list()
-  kvals <- Cvals
+  kvals <- 2^seq(-6, 2, by=1)
   model.df <- expand.grid(C=Cvals, k.width=kvals)
   for(model.i in 1:nrow(model.df)){
     model <- model.df[model.i,]
     Cval <- model$C
     k.width <- model$k.width
-    ##ker <- rbfdot(k.width)
-    ker <- laplacedot(k.width)
+    ker <- rbfdot(k.width)
+    ##ker <- laplacedot(k.width)
     cat(sprintf("%4d / %4d C=%5.2f k.width=%5.2f\n",
                 model.i, nrow(model.df), Cval, k.width))
-    fit <- softCompareQP(Pairs, ker, C=Cval)
+    fit <- softCompareQP(Pairs.train, ker, C=Cval)
     models[[model.i]] <- fit
     ##X.grid$rank <- fit$rank(as.matrix(X.grid))
     ## pretty good train err:
-    yhat <- fit$predict(Xi, Xip)
+    yhat <- with(Pairs, fit$predict(Xi, Xip))
+    yi <- Pairs$yi
     table(yi, yhat)
     sets <- list(train=is.train, test=!is.train)
     for(set in names(sets)){
@@ -71,22 +106,30 @@ for(norm in names(pair.sets)){
     }
   }
 ## train/test error curves.
-library(grid)
-ggplot(err.df)+
-  geom_line(aes(log2(Cval), error, group=set, colour=set))+
-  facet_wrap("k.width")+
-  theme_bw()+
-  theme(panel.margin=unit(0,"cm"))
-
   test.err <- subset(err.df, set=="test")
   chosen <- which.min(test.err$error)
+  ## if the optimal model occurs on the min/max of the tested
+  ## hyperparameters, then this is probably sub-optimal and we need to
+  ## define a larger grid.
+  overfitPlot <- ggplot(err.df, aes(log2(Cval), error))+
+    geom_line(aes(group=set, colour=set))+
+    facet_wrap("k.width")+
+    theme_bw()+
+    theme(panel.margin=unit(0,"cm"))+
+    geom_point(data=test.err[chosen,])
+  print(overfitPlot)
+  stopifnot(test.err[chosen,"Cval"] != range(Cvals))
+  stopifnot(test.err[chosen,"k.width"] != range(kvals))
+
   fit <- models[[chosen]]
   r <- fit$rank(X.grid)
   f <- funs[[norm]]
   rank.df <- rbind(data.frame(X.grid, rank=r-min(r), what="learned"),
                    data.frame(X.grid, rank=apply(X.grid, 1, f), what="latent"))
-  ggplot(rank.df, aes(x1, x2, z=rank))+
-    geom_contour(aes(colour=..level.., linetype=what, group=what))
+  normContour <- ggplot(rank.df, aes(x1, x2, z=rank))+
+    geom_contour(aes(colour=..level.., linetype=what, group=what))+
+    coord_equal()
+  print(normContour)
   all.ranks <- rbind(all.ranks, data.frame(rank.df, norm))
 }
 labels <- c(l1="||x||_1^2",
