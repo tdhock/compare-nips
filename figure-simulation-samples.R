2cea281507613adf506a2577725fece63abce951
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-10-08 18:01:49 +0900

    vary N and seed in sim

---
+++ figure-simulation-samples.R
@@ -0,0 +1,19 @@
+works_with_R("3.0.2", plyr="1.8")
+
+source("tikz.R")
+
+load("simulation.samples.RData")
+err <- simulation.samples$err
+err$percent <- err$error / err$count * 100
+percents <-
+  ddply(err, .(N, fit.name, norm), summarize,
+        mean=mean(percent),
+        sd=sd(percent))
+library(grid)
+boring <- ggplot(percents, aes(N, mean, group=fit.name))+
+  geom_ribbon(aes(ymin=mean-sd,ymax=mean+sd,fill=fit.name),alpha=1/2)+
+  geom_line(aes(colour=fit.name))+
+  facet_grid(.~norm)+
+  theme_bw()+
+  theme(panel.margin=unit(0,"cm"))
+print(boring)
