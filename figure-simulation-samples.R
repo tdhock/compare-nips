c0f4294180b78299d46979852a7e28ed6c392084
Toby Dylan Hocking <toby@sg.cs.titech.ac.jp>
2013-10-15 12:04:40 +0900

    show sd instead of se

--- figure-simulation-samples.R
+++ figure-simulation-samples.R
@@ -9,17 +9,22 @@ err$percent <- err$error / err$count * 100
 percents <-
   ddply(err, .(N, fit.name, norm), summarize,
         mean=mean(percent),
+        sd=sd(percent),
         se=sd(percent)/sqrt(length(percent)))
 library(grid)
 percents$fit.name <- factor(percents$fit.name, c("rank", "compare"))
 labels <- c(l1="||x||_1^2",
             l2="||x||_2^2",
             linf="||x||_\\infty^2")
-percents$label <- sprintf("$r(x) = %s$", labels[as.character(percents$norm)])
+makelabel <- function(x)sprintf("$r(x) = %s$", labels[as.character(x)])
+percents$label <- makelabel(percents$norm)
+err$label <- makelabel(err$norm)
 leg <- "SVM\ntype"
 boring <- ggplot(percents, aes(N, mean, group=fit.name))+
-  geom_ribbon(aes(ymin=mean-se,ymax=mean+se,fill=fit.name),alpha=1/2)+
+  geom_ribbon(aes(ymin=mean-sd,ymax=mean+sd,fill=fit.name),alpha=1/2)+
   geom_line(aes(colour=fit.name),lwd=1.5)+
+  ## Plot actual data:
+  ##geom_point(aes(N, error/count*100, colour=fit.name), data=err)+
   facet_grid(.~label)+
   theme_bw()+
   theme(panel.margin=unit(0,"cm"))+
